ca7c8242b7bc0c6992e9a4c4f1f90483227fb52a
Josch <josuechoc4567@gmail.com>
2024-02-14 22:41:40 -0600

    [add] Dashboard Histórico
    * Endpoint

--- Practica 1/Backend/src/router/router.js
+++ Practica 1/Backend/src/router/router.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const dashboardPrincipal = require('../controllers/principal')
+const dashboardHistorial = require('../controllers/historico')
+
 
 router
     .get("/espacionOcupados",dashboardPrincipal.getLugaresPaquero)
@@ -8,4 +10,7 @@ router
     .get("/vehiculoRol",dashboardPrincipal.getVehiculoRol)
     .get("/ingresosDia",dashboardPrincipal.getIngresodeldia)
     .get("/egresosDia",dashboardPrincipal.getEgresosdedia)
+    .get("/historial_ingresosEgresos",dashboardHistorial.getHistoriaIngresoEgresos)
+    .get("/historial_VehiducloRol",dashboardHistorial.getHistoriaVehiculoRol)
+    .get("/historial_Personas",dashboardHistorial.getHistoriaIngresosPersonas)
 module.exports = router
